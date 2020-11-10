4bbf8a4223e8538c05345d29c670893723ed3c67
Dugald Steer <dugsteer@gmail.com>
2020-11-10 12:03:17 +0100

    bingo caller

--- script.js
+++ script.js
@@ -1,91 +1,145 @@
-const menu = document.getElementById("menu");
-const sidey = document.getElementById("conto");
-const blue = window.matchMedia("(max-width: 800px)");
-
-function myFunction(x) {
-  if (blue.matches) {
-    sidey.classList.remove("block");
-  }
-}
-
-function toggler() {
-  sidey.classList.toggle("block");
-}
-
-myFunction(blue);
-
-//CHANGE CARDS
-const count = document.getElementById("count");
-let imageArt = document.getElementById("image-art");
-let imageText = document.getElementById("image-text");
-
+// LOADER, SIDEBAR and ABOUT INFO
+const sidey = document.getElementById('conto');
 const loader = document.getElementById("loaderDiv");
+const extra = document.getElementById('info');
+const blue = window.matchMedia("(max-width: 800px)");
+const imgdiv = document.getElementById('imgdiv');
+const title = document.getElementById('title');
+const container = document.getElementById('container');
 
-function stopLoader() {
-  loader.classList.add("stop");
-}
+// Show and hide about info
+function show(){info.classList.toggle('xxc');};
+function unshow(){info.classList.remove('xxc');};
 
+//Center the loader wherever the page is
+function checkForTop (){loader.style.top = (window.pageYOffset) + "px";}
+checkForTop();
+
+//Manage the loader
+function stopLoader() {loader.classList.add("stop");}
+function startLoader(){loader.classList.remove("stop"); setTimeout(stopLoader, 1000);}
 setTimeout(stopLoader, 1000);
 
-var imageList = [
-  "bat.jpg",
-  "black-cat.jpg",
-  "broomstick.jpg",
-  "candy.jpg",
-  "cauldron.jpg",
-  "clown.jpg",
-  "fancy-dress.jpg",
-  "Frankenstein.jpg",
-  "ghost.jpg",
-  "monster.jpg",
-  "moon.jpg",
-  "mummy.jpg",
-  "pumpkin.jpg",
-  "skeleton.jpg",
-  "skull.jpg",
-  "vampire.jpg",
-  "werewolf.jpg",
-  "witch.jpg",
-  "wizard.jpg",
-  "zombie.jpg",
-];
+//Stop the sidebar appearing on small screens with matchmedia
+function myFunction(x) {if (blue.matches) {sidey.classList.remove("block");}}
+function toggler() {sidey.classList.toggle("block");}
+myFunction(blue);
 
-let y = imageText.innerHTML;
-let z = y.concat(".jpg");
-let x = imageList.indexOf(z);
 
-function goForwards() {
-  if (x < imageList.length - 1) {
-    x++;
-    imageArt.src = `/img/${imageList[x]}`;
-    imageText.textContent = `${imageList[x].slice(0, -4)}`;
-    count.innerHTML = `${x + 1}/${imageList.length}`;
-    return x;
-  } else {
-    imageArt.src = ` /img/${imageList[imageList.length - 1]}`;
-    imageText.textContent = `${imageList[imageList.length - 1].slice(0, -4)}`;
-    count.innerHTML = `${imageList.length}/${imageList.length}`;
-    x = 0;
-    x = imageList.length - 1;
-    return x;
+//GENERAL SCRIPT
+const bingoArray = [
+  "bat.svg", "bone.svg", "broomstick.svg", "candles.svg", "candy.svg", "cat.svg", "cauldron.svg", "clown.svg", "coffin.svg", "death.svg", "fancy-dress.svg", "fangs.svg", "frankenstein.svg", "ghost.svg", "grave.svg", "halloween.svg", "hat.svg", "monster.svg", "moon.svg", "mummy.svg", "pirate.svg", "pumpkin.svg", "skeleton.svg", "skull.svg", "spell.svg", "spider.svg", "vampire.svg", "wand.svg", "web.svg", "werewolf.svg", "witch.svg", "wizard.svg", "zombie.svg"  ];
+
+  const gameboard = document.getElementById("gameboard");
+  
+  const newArray = [];
+  
+  function shuffle(bingoArray) {
+    var j, x, i;
+    for (i = bingoArray.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = bingoArray[i];
+      bingoArray[i] = bingoArray[j];
+      bingoArray[j] = x;
+      clicker2.innerHTML = `${bingoArray.length} to go`;
+
+    }
+    return bingoArray;
   }
-}
-
-function goBackwards() {
-  if (x > 0) {
-    x--;
-    imageArt.src = `/img/${imageList[x]}`;
-    imageText.textContent = `${imageList[x].slice(0, -4)}`;
-    count.innerHTML = `${x + 1}/${imageList.length}`;
-    return x;
-  } else {
-    imageArt.src = ` /img/${imageList[0]}`;
-    imageText.textContent = `${imageList[0].slice(0, -4)}`;
-    count.innerHTML = `${1}/${imageList.length}`;
-    x = 0;
-    return x;
+  
+  const button0 = document.getElementById("button0");
+  const button1 = document.getElementById("button1");
+  const button2 = document.getElementById("button2");
+  const clicker2 = document.getElementById("clicker2");
+  
+  function bingoCall() {
+    button1.style.display = "none";
+    button2.style.display = "none";
+    imgdiv.style.display = "none";
+    title.style.display = "none";
+    container.style.gridTemplateRows = "auto minmax(20rem, auto)"
+    gameboard.classList.add("unCenter");
+    gameboard.innerHTML = "";
+    const oldArray = bingoArray;
+    var blue = oldArray.shift();
+    clicker2.innerHTML = oldArray.length > 0 ? `${oldArray.length} to go` : `RESET`;
+    newArray.push(blue);
+    fillHTML();
   }
-}
-
-goForwards();
-goBackwards();
+  
+  function bingoCall1() {
+    button0.style.display = "none";
+    button2.style.display = "none";
+    imgdiv.style.display = "none";
+    title.style.display = "none";
+    container.style.gridTemplateRows = "10rem auto;"
+    gameboard.classList.add("unCenter");
+    gameboard.innerHTML = "";
+    const oldArray = bingoArray;
+    var blue = oldArray.shift();
+    clicker2.innerHTML = oldArray.length > 0 ? `${oldArray.length} to go` : `RESET`;
+    newArray.push(blue);
+    fillHTML1();
+    }
+  
+  
+  function bingoCall2() {
+    button1.style.display = "none";
+    button0.style.display = "none";
+    imgdiv.style.display = "none";
+    title.style.display = "none";
+    container.style.gridTemplateRows = "10rem auto;"
+    gameboard.classList.add("unCenter");
+    gameboard.innerHTML = "";
+    const oldArray = bingoArray;
+    var blue = oldArray.shift();
+    clicker2.innerHTML = oldArray.length > 0 ? `${oldArray.length} to go` : `RESET`;
+    newArray.push(blue);
+    fillHTML2();
+  }
+  
+  function fillHTML() {
+    // for (var i=0; i<newArray.length;i++){
+    //   gameboard.innerHTML =
+    newArray.forEach(function (item) {
+      var div = document.createElement("div");
+      div.innerHTML = `
+    <div class="minicard text-center">
+    <img src="./img/${item}">
+    <h3 class="minicard__h3">${item.slice(0, -4)}</h3>
+  </div>
+  `;
+      gameboard.appendChild(div);
+    });
+  }
+  
+  function fillHTML1() {
+    newArray.forEach(function (item) {
+      let div = document.createElement("div");
+      (item.length < 2) ? console.log('no') : 
+      div.innerHTML = `
+    <div class="minicard text-center d-flex justify-content-center">
+    <img src="./img/${item}">
+  </div>
+  `;
+      gameboard.appendChild(div);
+    });
+  }
+  
+  
+  function fillHTML2() {
+    // for (var i=0; i<newArray.length;i++){
+    //   gameboard.innerHTML =
+    newArray.forEach(function (item) {
+      var div = document.createElement("div");
+      div.innerHTML = `
+    <div class="minicard text-center d-flex justify-content-center">
+    <h3 class="minicard__h3">${item.slice(0, -4)}</h3>
+  </div>
+  `;
+      gameboard.appendChild(div);
+    });
+  }
+  
+  
+  shuffle(bingoArray);
